c283bbbf1f8b8b271d54386bfa38aa86ede2ce5d
kevCastillo19 <sypecr@gmail.com>
2021-10-19 23:09:12 -0600

    Iniciando backend

---
+++ Backend/btomail/src/main/java/com/btomail/app/BtomailApplication.java
@@ -0,0 +1,13 @@
+package com.btomail.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BtomailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BtomailApplication.class, args);
+	}
+
+}
---
+++ Backend/btomail/src/test/java/com/btomail/app/BtomailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.btomail.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BtomailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
